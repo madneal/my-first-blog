566166c4c2f5f19f025eb663b62445abbb3e6bd0
neal1991 <neal1991@sina.com>
2015-10-21 12:44:42 +0800

    modift the post_list.html

---
+++ blog/migrations/0002_auto_20151014_1549.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='created_date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 14, 15, 49, 22, 535522, tzinfo=utc)),
+        ),
+    ]
