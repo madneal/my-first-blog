80f926e553c5737803e7aca749b1ef1e5ddf8aed
neal1991 <neal1991@sina.com>
2015-10-30 21:20:08 +0800

    the commit

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'$',views.post_list,name='post_list')
+    url(r'$',views.post_list,name='post_list'),
+    # url(r'^index/',views.index,name='index'),
+    # url(r'$',views.index,name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,6 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(created_date=timezone.now()).order_by('published_date')
     return render(request,'blog/post_list.html',{'posts':posts})
+
+def index(request):
+    return render(request,'blog/index.html',{})
