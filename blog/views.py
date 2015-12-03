bad52526322b523dd79f6146c2e4c0f1f99a0af4
neal1991 <neal1991@sina.com>
2015-12-03 12:05:17 +0800

    my-cv

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(created_date=timezone.now()).order_by('published_date')
-    return render(request,'blog/post_list.html',{'posts':posts})
+    return render(request,'blog/cv.html',{'posts':posts})
 
 def index(request):
     return render(request,'blog/index.html',{})
