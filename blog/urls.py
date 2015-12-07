c6b8b92644166ed1bce21dac1531c8942d018e67
neal1991 <neal1991@sina.com>
2015-12-07 20:43:26 +0800

    modify url

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.post_list,name='post_list'),
+    #url(r'^$',views.post_list,name='post_list'),
     #url(r'',views.index,name='index'),
     url(r'^cv/',views.cv),
     url(r'^cv_zh/',views.cv_zh),
