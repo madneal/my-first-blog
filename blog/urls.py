672edc264a0c217c597c91a6773627d1d39ab78d
neal1991 <neal1991@sina.com>
2015-12-07 20:32:34 +0800

    add the main index page

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #url(r'$',views.post_list,name='post_list'),
-    url(r'$',views.index,name='index'),
-    url(r'^cv',views.cv,name='cv'),
-    url(r'^cv_zh',views.cv_zh,name='cv_zh'),
+    url(r'^$',views.post_list,name='post_list'),
+    #url(r'',views.index,name='index'),
+    url(r'^cv/',views.cv),
+    url(r'^cv_zh/',views.cv_zh),
     # url(r'^index/',views.index,name='index'),
     # url(r'$',views.index,name='index'),
 ]
