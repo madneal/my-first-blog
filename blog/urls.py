7dac93d6555407e7d298b0e3d090b070607dd446
neal1991 <neal1991@sina.com>
2015-12-07 20:47:31 +0800

    modify url

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     #url(r'^$',views.post_list,name='post_list'),
-    #url(r'',views.index,name='index'),
+    url(r'^$',views.index,name='index'),
     url(r'^cv/',views.cv),
     url(r'^cv_zh/',views.cv_zh),
     # url(r'^index/',views.index,name='index'),
