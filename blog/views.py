058777b2fa55d0f1962724479c6d99bb0a159e5f
neal1991 <neal1991@sina.com>
2015-12-07 17:18:24 +0800

    add different versions of cv

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,11 @@ def post_list(request):
     posts = Post.objects.filter(created_date=timezone.now()).order_by('published_date')
     return render(request,'blog/cv.html',{'posts':posts})
 
+def cv(request):
+    return  render(request,'blog/cv.html',{})
+
+def cv_zh(request):
+    return render(request,'blog/cv_zh.html',{})
+
 def index(request):
     return render(request,'blog/index.html',{})
