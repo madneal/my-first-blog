485f1b9574e0ff426c1f3c1aa4871b08b7105c05
neal1991 <neal1991@sina.com>
2015-12-25 20:48:46 +0800

    modify the homepage button and download the cv button; with the add of bootstrap.min.css

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^cv/',views.cv),
     url(r'^cv_zh/',views.cv_zh),
     url(r'^download_file/',views.download_file),
+    url(r'^download_cv/',views.download_cv),
     # url(r'^index/',views.index,name='index'),
     # url(r'$',views.index,name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+#  -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
@@ -31,12 +33,29 @@ def download_file(request):
                 else:
                     break
 
-    #the_filename = "cv.pdf"
+    the_filename = "cv.pdf"
     response = StreamingHttpResponse(file_iterator(filename))
     response['Content-Type'] = 'application/octet-stream'
-    response['Content-Disposition'] = 'attachment;filename="{0}'.format('cv.pdf')
+    response['Content-Disposition'] = 'attachment;filename="{0}"'.format('cv.pdf')
 
     return response
+
+def download_cv(request):
+    BASE = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(BASE,'static/media/中文简历.pdf')
+    def file_iterator(filename,chunk_size=512):
+        with open(filename) as f:
+            while True:
+                c = f.read(chunk_size)
+                if c:
+                    yield c
+                else:
+                    break
+    response = StreamingHttpResponse(file_iterator(filename))
+    response['Content-Type'] = 'application/octet-stream'
+    response['Content-Disposition'] = 'attachment;filename="{0}"'.format('中文简历.pdf')
+
+    return  response
 
 def output_pdf(request):
     response = HttpResponse(content_type='application/pdf')
