b9e3194ebb5407ca9c957ea0ceefc690789755d3
neal1991 <neal1991@sina.com>
2016-07-12 22:55:47 +0800

    remove the module

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^weixin',include('wexin.urls')),
     url(r'',include('blog.urls')),
 ]
--- weixin/forms/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*-coding:utf-8 -*-
-"""
-Created on 2015-01-07
-
-@author: Danny
-DannyWork Project
-"""
-
-from .message import *
