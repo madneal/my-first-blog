dfd3abed2a2120e6a25bf0cd3f6f891cbc997314
neal1991 <neal1991@sina.com>
2018-04-03 20:13:49 +0800

    disable debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k^a0llvysc!*m&t0u48_-#xos%q1zl@s6+y71j$nm6*oevfnx0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
